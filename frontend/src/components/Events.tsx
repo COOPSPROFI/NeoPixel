67acb8d71cec224898f1dd0cbafaed064675ce56
agd01991 <agd01991@gmail.com>
2022-12-10 23:15:40 +0300

    test

--- frontend/src/components/Events.tsx
+++ frontend/src/components/Events.tsx
@@ -2,7 +2,7 @@ export default function Events() {
 	return (
 		<div className='bg-[#171616] py-[50px] laptop:py-[100px]'>
 			<div className="max-w-[1400px] mx-auto">
-				1234
+				1234test
 			</div>
 		</div>
 	)
