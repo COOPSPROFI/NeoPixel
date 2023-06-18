310c3562de135c3a45e24782a7c9aa958b60861e
agd01991 <agd01991@gmail.com>
2023-06-18 21:27:59 +0300

    Add Edit Event

--- backend/internal/model/event.go
+++ backend/internal/model/event.go
@@ -3,9 +3,9 @@ package model
 type Event struct {
 	ID          int64  `db:"id" json:"id"`
 	Title       string `db:"title" json:"title"`
-	Description string `db:"desc" json:"desc"`
+	Description string `db:"description" json:"description"`
 	Date        string `db:"date" json:"date"`
-	Img         string `db:"src" json:"src"`
+	Img         string `db:"img" json:"img"`
 }
 type CreateEvent struct {
 	Title       string `json:"title"`
@@ -14,6 +14,10 @@ type CreateEvent struct {
 	Img         string `json:"img"`
 }
 type UpdateEvent struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Date        string `json:"date"`
+	Img         string `json:"img"`
 }
 type DeleteEvent struct {
 	ID int64 `json:"id"`
