c0d8305bf0a9bf1482de93fac987f815f58b816f
Дмитриев Андрей <agd01991@gmail.com>
2023-11-10 17:04:09 +0300

    robit

--- frontNext/src/pages/Login.tsx
+++ frontNext/src/pages/Login.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react';
 import Login from '../components/Login'
 import '../styles/Home.module.css'
 
 export default function Home() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <div>
-      <Login/>
+      <Login setIsAuthenticated={setIsAuthenticated} />
     </div>
   )
 }
