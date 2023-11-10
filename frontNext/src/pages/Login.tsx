ca1508db11131e06399c516cccd39cb987025a27
Дмитриев Андрей <agd01991@gmail.com>
2023-11-10 17:43:06 +0300

    Revert "robit"
    This reverts commit 7b7753eb5ebfcd7cd9aa8dbae649673934cb6362.

--- frontNext/src/pages/Login.tsx
+++ frontNext/src/pages/Login.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react';
 import Login from '../components/Login'
 import '../styles/Home.module.css'
 
 export default function Home() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
   return (
     <div>
-      <Login setIsAuthenticated={setIsAuthenticated} />
+      <Login/>
     </div>
   )
 }
